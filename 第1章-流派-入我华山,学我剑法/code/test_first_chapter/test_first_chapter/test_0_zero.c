43e97a1959ae0a0d200412c02f2ab377cb46bd7a
wangzhi <wangzhione@163.com>
2017-07-03 21:01:49 +0800

    提交第一章

--- 第1章-流派-入我华山,学我剑法/code/test_first_chapter/test_first_chapter/test_0_zero.c
+++ 第1章-流派-入我华山,学我剑法/code/test_first_chapter/test_first_chapter/test_0_zero.c
@@ -5,12 +5,19 @@
 
 #define test_0_zero main
 
+enum TCHARS {
+	TA,
+	TB
+};
+
 int test_0_zero(void) {
 
 	void * ptr = NULL;
 	bool flag = false;
 
 	char zero = '0';
+
+	printf("sizeof(enum TCHARS) = %zu.\n", sizeof(enum TCHARS));
 
 	return EXIT_SUCCESS;
 }
